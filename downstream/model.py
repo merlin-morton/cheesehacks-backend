46037ee3f270384b8bdb372b0f7ae185e87daaac
markstanl <markgstanley1@gmail.com>
2026-03-01 08:07:18 -0600

    model update

--- downstream/model.py
+++ downstream/model.py
@@ -2,29 +2,49 @@ import torch
 import torch.nn as nn
 
 from model import SharedEncoderBinaryHeads
+from huggingface_hub import hf_hub_download
 
 
 class CoolProjectionHead(nn.Module):
     """
-    Maps the embedded space of moral vector space into some arbitrary
+    maps the embedded space of moral vector space into some arbitrary
     downstream task space.
-
-    Args:
-        in_features: dimension of the moral manifold.
-        out_classes: number of target classes
     """
 
     def __init__(self,
-                 in_features: int = 384,
+                 in_features: int = 768,
                  out_classes: int = 16,
                  checkpoint_path: str = None):
         super().__init__()
-        encoder = SharedEncoderBinaryHeads(input_dim=in_features, latent_dim=in_features, tasks=['dummy'])
-        # freeze encoder
-        for param in encoder.parameters():
-            param.requires_grad = False
 
-        # mlp
+        # backbone
+        if checkpoint_path is None:
+            # download from hub if no local path provided
+            downloaded_path = hf_hub_download(
+                repo_id="Praneet-P/ethics-multihead-model",
+                filename="checkpoints/shared_encoder_heads.pt"
+            )
+            checkpoint = torch.load(downloaded_path, map_location="cpu")
+
+            self.encoder = SharedEncoderBinaryHeads(
+                input_dim=checkpoint['input_dim'],
+                latent_dim=checkpoint['latent_dim'],
+                tasks=checkpoint['tasks']
+            )
+            self.encoder.load_state_dict(checkpoint["model_state"])
+            in_features = checkpoint['latent_dim']
+        else:
+            self.encoder = SharedEncoderBinaryHeads(
+                input_dim=in_features,
+                latent_dim=in_features,
+            )
+
+        # freeze backbone
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+        self.encoder.eval()
+
+        # projection mlp
         self.mlp = nn.Sequential(
             nn.Linear(in_features, out_classes * 2),
             nn.GELU(),
@@ -34,10 +54,9 @@ class CoolProjectionHead(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
-        Args:
-            x: aggregated normative ethical vector. shape: [B, 5].
-
-        Returns:
-            raw class logits. shape: [B, out_classes].
+        :param x: input embeddings [B, transformer_features]
+        :returns: task logits [B, out_classes]
         """
-        return self.mlp(x)
+        # we suppose x are preencoded into the embeddings
+        features = self.encoder(x)
+        return self.mlp(features)
